1f5ecfd5bac1e222bab741885e5fe69fae1a4eff
Ulathi Ncube <lathincube@gmail.com>
2026-09-24 22:49:16 +0200

    Feat: add Categories

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,28 @@
+import styles from "./Categories.module.css"
+import { Link } from "react-router"
+
+function Categories() {
+  return (
+    <article className={styles.wrapper}>
+      <ul className={styles.categories}>
+        <li className={styles.category}>
+          <Link to="/" className={styles.link}>
+            Men
+          </Link>
+        </li>
+        <li className={styles.category}>
+          <Link to="/" className={styles.link}>
+            Women
+          </Link>
+        </li>
+        <li className={styles.category}>
+          <Link to="/" className={styles.sale}>
+            Sale
+          </Link>
+        </li>
+      </ul>
+    </article>
+  )
+}
+
+export default Categories
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import styles from "./Home.module.css"
 import Header from "../../components/Header"
+import Categories from "../../components/Categories"
 
 function Home() {
   return (
     <>
       <Header />
+      <Categories />
     </>
   )
 }
