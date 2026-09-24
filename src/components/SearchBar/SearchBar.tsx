00da29f7567d722dbd5ff7ff634270343779edb5
Ulathi Ncube <lathincube@gmail.com>
2026-09-24 23:00:36 +0200

    Feat:  add svg icon to SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,13 @@
 import styles from "./SearchBar.module.css"
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 
 function SearchBar() {
   return (
     <article className={styles.wrapper}>
       <form className={styles.form}>
+        <div className={styles.container}>
+          <MagnifyingGlassIcon className={styles.icon} />
+        </div>
         <div className={styles.group}>
           <input
             type="search"
